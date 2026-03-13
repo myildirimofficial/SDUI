563fbe77eec3e1a0833db20bfae98b5ac3c21b0f
SDClowen <mahmutyildirim@outlook.com.tr>
2026-03-13 15:28:20 +0300

    ....

---
+++ SDUI/Rendering/RendererFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SDUI.Rendering;
+
+internal static class RendererFactory
+{
+    internal static IWindowRenderer CreateRenderer(RenderBackend backend, IntPtr hwnd)
+    {
+        IWindowRenderer renderer = backend switch
+        {
+            RenderBackend.Software => throw new NotSupportedException($"{backend} backend does not supporting on this platform!"),
+            RenderBackend.OpenGL => throw new NotSupportedException($"{backend} backend does not supporting on this platform!"),
+            RenderBackend.DirectX11 => throw new NotSupportedException($"{backend} backend does not supporting on this platform!"),
+            RenderBackend.Vulkan => throw new NotSupportedException($"{backend} backend does not supporting on this platform!"),
+            RenderBackend.Metal => throw new NotSupportedException($"{backend} backend does not supporting on this platform!"),
+            _ => throw new NotSupportedException($"{backend} backend does not supporting on this platform!")
+        };
+
+        renderer.Initialize(hwnd);
+        return renderer;
+    }
+}
